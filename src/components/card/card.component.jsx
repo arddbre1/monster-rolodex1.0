4a25345257408fc958a94cb4328b089e11e8a0f2
arddbre1 <ardianidizi@gmail.com>
2022-07-29 11:23:36 +0200

    monster rolodex

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+
+import './card.styles.css';
+
+class Card extends Component {
+  render() {
+    const { id, name, email, username } = this.props.monster;
+
+    return (
+      <div className="card-container" key={id}>
+        <img
+          alt={`monster ${name}`}
+          src={`https://robohash.org/${id}?set=set2=180x180`}
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+        <p>{username}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
